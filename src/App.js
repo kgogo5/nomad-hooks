f2692a4dd9a2ea2996195d3075bbf0fba4d0e545
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-09-12 03:22:34 +0900

    reducer로 count 기능 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <>
-      <h1>To Dos</h1>
+      <h1>count : {state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>+</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>-</button>
     </>
   );
 }
