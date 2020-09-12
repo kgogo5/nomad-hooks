bfbb8cb9caee830c8b90a512cc25d8cd4df6497a
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-09-13 00:35:42 +0900

    toDo list 추가 제거 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer } from "react";
+import { v4 as uuid } from "uuid";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+const initialState = {
+  toDos: [],
+};
+
+const ADD = "add";
+const DEL = "delete";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+    case ADD:
+      return {
+        toDos: [...state.toDos, { text: action.newToDo, id: uuid() }],
+      };
+    case DEL:
+      return {
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.delCnt),
+      };
     default:
-      throw new Error();
+      return;
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if (newToDo === "") {
+      return console.log("할 일을 적어주세요");
+    }
+    dispatch({ type: ADD, newToDo });
+    setNewToDo("");
+  }
+
+  function onChange(e) {
+    setNewToDo(e.target.value);
+  }
+
   return (
     <>
-      <h1>count : {state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>+</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>-</button>
+      <h1>오늘의 할일</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="오늘의 할 일을 적어주세요"
+          onChange={onChange}
+          value={newToDo}
+        />
+      </form>
+      <ul>
+        {state.toDos.map((props) => {
+          return (
+            <li key={props.id}>
+              <span>{props.text}</span>{" "}
+              <button onClick={() => dispatch({ type: DEL, delCnt: props.id })}>
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
